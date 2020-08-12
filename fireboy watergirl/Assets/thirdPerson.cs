bd73724ba4e01606445f69ca04def497f928da59
Vu Thanh Tung <vuthanhtung2016hn@gmail.com>
2020-08-12 18:19:06 +0700

    add the fireboy watergame from tung's computer

---
+++ fireboy watergirl/Assets/thirdPerson.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdPerson : MonoBehaviour
+{
+    private Rigidbody body;
+    private Transform player;
+    public Transform head;
+    public float speed;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float soleThickness;
+    private Vector3 moveDir;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        //body.AddForce(-transform.up* 1000f);
+        body.velocity = Vector3.down * 100f;
+        Debug.Log(transform.InverseTransformDirection(body.velocity));
+        Debug.Log(transform.up);
+    }
+}
